5bbebee7526f81195583ffc6644ec03e600d59a9
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-27 15:01:01 +0300

    test buttons

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class WebHookController extends Controller
 {
     public function index(Request $request){
         $path = "https://api.telegram.org/bot1955140014:AAE0KkWUJzKP6fnCmX2UsJ0iQocFz8FYG10";
-//        dd($request);
         $request = $request->toArray();
         $chatId = (int)trim($request["message"]["chat"]["id"]);
-        $name = $request["message"]["from"]["username"];
+        $name = $request["message"]["from"]["first_name"];
         $chatId = (int)preg_replace('/\^ /', "", $chatId);
-//        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in");
-        Http::post($path."/sendmessage?chat_id=".$chatId."&text=Hello ".$name);
+        $text = "Hello ".$name;
+        $text = "keyboardButton#a2fa4880 text:string = KeyboardButton;";
+        Http::post($path."/sendmessage?chat_id=".$chatId."&text=".$text);
     }
 }
