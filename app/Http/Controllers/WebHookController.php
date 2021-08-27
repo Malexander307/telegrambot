59d74f3b0d498f4e5a29223fa6d41032f31cd094
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-27 12:50:58 +0300

    test message

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -13,9 +13,8 @@ class WebHookController extends Controller
 //        dd($request);
         $request = $request->toArray();
         $chatId = (int)trim($request["message"]["chat"]["id"]);
-        $chatId = preg_replace('/\^ /', "", $chatId);
-        dd("test");
-        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in");
-//        Http::post($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ");
+        $chatId = (int)preg_replace('/\^ /', "", $chatId);
+//        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in");
+        Http::post($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ");
     }
 }
