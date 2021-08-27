618ed94bf92f26e3d5b02af1879937e016eaf858
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-27 11:48:55 +0300

    test message

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -11,7 +11,7 @@ class WebHookController extends Controller
     public function index(Request $request){
         $path = "https://api.telegram.org/bot1955140014:AAE0KkWUJzKP6fnCmX2UsJ0iQocFz8FYG10";
         $request = $request->toArray();
-
+        dd($request);
         $chatId = $request["message"]["chat"]["id"];
         file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in");
 //        Http::post($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ");
