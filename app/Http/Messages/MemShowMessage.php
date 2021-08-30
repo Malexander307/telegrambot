5e26ad2ce40ad70f89134b7eb027125c18e87e30
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-30 12:30:23 +0300

    show mems

--- app/Http/Messages/MemShowMessage.php
+++ app/Http/Messages/MemShowMessage.php
@@ -16,7 +16,7 @@ class MemShowMessage
         $encodedKeyboard = json_encode($keyboard);
         $data = array(
             'chat_id' => (int)trim($chat_id),
-            'photo' => $mem->photo_id,
+            'photo' => $mem->image_id,
             'reply_markup' => $encodedKeyboard
         );
         Messenger::send('sendPhoto', $data);
