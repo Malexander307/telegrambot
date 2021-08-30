24a792ec418a24b2d6168e396d40fe30c3ccedeb
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-30 10:32:48 +0300

    add message for adding of mems

--- app/Http/Messages/AddMessage.php
+++ app/Http/Messages/AddMessage.php
@@ -7,9 +7,10 @@ class AddMessage
     public static function addMemsMessage($request){
         $text = "Send a mem as picture";
         $data = array(
-                    'chat_id' => (int)trim($request["message"]["chat"]["id"]),
+                    'chat_id' => (int)trim($request['callback_query']['from']['id']),
                     'text' => $text
                 );
         Messenger::send('sendmessage', $data);
+        Http::post($path . "/answerCallbackQuery?callback_query_id=". $request['callback_query']['id']);
     }
 }
