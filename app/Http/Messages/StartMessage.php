cb1fef3b3dac8085631ed4e5be7fa04e798f1b75
Malexadner307 <o.moskalyk@grt-team.com>
2021-08-30 10:13:08 +0300

    added buttons

--- app/Http/Messages/StartMessage.php
+++ app/Http/Messages/StartMessage.php
@@ -6,9 +6,19 @@ class StartMessage
 {
     public static function firstMessage($request){
         $text = "Hello " . $request["message"]["from"]["first_name"] . " i am mems bot";
+        $keyboard = [
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'Add mem', 'callback_data' => 'add_mem'],
+                        ['text' => 'Watch mems', 'callback_data' => 'watch_mems']
+                    ],
+                ]
+            ];
+        $encodedKeyboard = json_encode($keyboard);
         $data = array(
                     'chat_id' => (int)trim($request["message"]["chat"]["id"]),
-                    'text' => $text
+                    'text' => $text,
+                    'reply_markup' => $encodedKeyboard
                 );
         Messenger::send('sendmessage', $data);
     }
